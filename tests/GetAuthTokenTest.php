b131869c4e9bd15b0c021b499e00994f3fac6be4
Ivan Stasiuk <brokeyourbike@gmail.com>
2021-10-22 12:20:53 +0300

    test: cover more edge cases

--- tests/GetAuthTokenTest.php
+++ tests/GetAuthTokenTest.php
@@ -61,4 +61,68 @@ class GetAuthTokenTest extends TestCase
                 3600
             );
     }
+
+    /** @test */
+    public function it_can_return_cached_value()
+    {
+        $tokenValue = 'super-secure-token';
+
+        $mockedConfig = $this->getMockBuilder(ConfigInterface::class)->getMock();
+
+        /** @var \Mockery\MockInterface $mockedClient */
+        $mockedClient = \Mockery::mock(\GuzzleHttp\Client::class);
+        $mockedClient->shouldNotReceive('request');
+
+        /** @var \Mockery\MockInterface $mockedCache */
+        $mockedCache = \Mockery::mock(CacheInterface::class);
+        $mockedCache->shouldReceive('has')
+            ->once()
+            ->andReturn(true);
+        $mockedCache->shouldReceive('get')
+            ->once()
+            ->andReturn($tokenValue);
+
+        /**
+         * @var ConfigInterface $mockedConfig
+         * @var \GuzzleHttp\Client $mockedClient
+         * @var CacheInterface $mockedCache
+         * */
+        $api = new Client($mockedConfig, $mockedClient, $mockedCache);
+        $requestResult = $api->getAuthToken();
+
+        $this->assertSame($tokenValue, $requestResult);
+    }
+
+    /**
+     * @test
+     * @dataProvider isLiveProvider
+     */
+    public function it_will_return_null_if_response_invalid()
+    {
+        $mockedConfig = $this->getMockBuilder(ConfigInterface::class)->getMock();
+
+        $mockedResponse = $this->getMockBuilder(\Psr\Http\Message\ResponseInterface::class)->getMock();
+        $mockedResponse->method('getStatusCode')->willReturn(200);
+        $mockedResponse->method('getBody')->willReturn('{}');
+
+        /** @var \GuzzleHttp\Client $mockedClient */
+        $mockedClient = $this->createPartialMock(\GuzzleHttp\Client::class, ['request']);
+        $mockedClient->method('request')->willReturn($mockedResponse);
+
+        /** @var \Mockery\MockInterface $mockedCache */
+        $mockedCache = \Mockery::spy(CacheInterface::class);
+
+        /**
+         * @var ConfigInterface $mockedConfig
+         * @var \GuzzleHttp\Client $mockedClient
+         * @var CacheInterface $mockedCache
+         * */
+        $api = new Client($mockedConfig, $mockedClient, $mockedCache);
+        $requestResult = $api->getAuthToken();
+
+        $this->assertNull($requestResult);
+
+        /** @var \Mockery\MockInterface $mockedCache */
+        $mockedCache->shouldNotReceive('set');
+    }
 }
